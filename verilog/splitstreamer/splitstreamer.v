cba45b7cae9597d95d682dd4ef4a7218733a2af4
Niek ten Brinke <niektb95@gmail.com>
2025-08-29 13:39:32 +0200

    Test button

--- verilog/splitstreamer/splitstreamer.v
+++ verilog/splitstreamer/splitstreamer.v
@@ -191,7 +191,7 @@ module system_management_unit
             
     assign read_en = ~empty && state_fclk; // Allow read if not empty and state is set
     assign write_en = ~full && state_fclk; // Allow write if not full and state is set
-    assign red = ~rst; // Red LED indicates lock status
+    assign red = user_sw_ff[1]; // Red LED indicates lock status
     assign blue = red; // Red LED indicates lock status
     assign green = red; // Red LED indicates lock status
 
